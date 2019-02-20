48a74fed69185ba4a34da18650557375166a6ab8
anton-zuev <ant0n3uev@gmail.com>
2019-02-20 09:51:34 +0200

    Minor changes

--- main.py
+++ main.py
@@ -24,19 +24,21 @@ def NaiveBayes(dataset, show=False):
     new_labels = gnb.fit(X, labels).predict(X)
 
     conf = confusion_matrix(labels, new_labels)
-    conf = conf / np.linalg.norm(conf)
+    print(conf)
+    conf = conf / conf.sum(axis=1)
     plt.matshow(conf)
+    plt.title("NaiveBayes")
     plt.show()
     if show:
         for i in range(len(X[0])):
-            if i >= 3:
+            if i >= 2:
                 break
             for j in range(i):
                 x = list(x[i] for x in X)
                 y = list(x[j] for x in X)
 
-                paint(x, y, labels)
-                paint(x, y, new_labels)
+                paint(x, y, labels, 'labels')
+                paint(x, y, new_labels, 'prediction')
 
         plt.show()
 
@@ -49,41 +51,49 @@ def GaussianClassifier(dataset, show=False):
     new_labels = classifier.predict(X)
 
     conf = confusion_matrix(labels, new_labels)
-    conf = conf / np.linalg.norm(conf)
+    print(conf)
+    conf = conf / conf.sum(axis=1)
 
     plt.matshow(conf)
+    plt.title("Gaussian")
     plt.show()
 
     if show:
         for i in range(len(X[0])):
-            if i >= 3:
+            if i >= 2:
                 break
             for j in range(i):
                 x = list(x[i] for x in X)
                 y = list(x[j] for x in X)
 
-                paint(x, y, labels)
-                paint(x, y, new_labels)
+                paint(x, y, labels, "labels")
+                paint(x, y, new_labels, "prediction")
 
         plt.show()
 
 
-def pca():
-    X, labels = load_iris(return_X_y=True)
-    pca = PCA(n_components=4)
+def pca(dataset, show=False):
+    X, labels = dataset
+
+    n = X.shape[1]
+
+    pca = PCA(n_components=n)
     pca.fit(X, labels)
     print(pca.score(X, labels))
     df1 = DataFrame(data=X)
     plt.matshow(df1.corr())
+    plt.title("Before PCA")
     df2 = DataFrame(data=pca.transform(X))
     plt.matshow(df2.corr())
+    plt.title("After PCA")
     plt.show()
 
 
-def paint(x, y, label):
+def paint(x, y, label, title=None):
     N = max(label) + 1
 
     fig, ax = plt.subplots(1, 1, figsize=(6, 6))
+    ax.set_title(title)
     cmap = plt.cm.jet
     cmaplist = [cmap(i) for i in range(cmap.N)]
     cmap = cmap.from_list('Custom cmap', cmaplist, cmap.N)
@@ -91,16 +101,18 @@ def paint(x, y, label):
     norm = matplotlib.colors.BoundaryNorm(bounds, cmap.N)
 
     scat = ax.scatter(x, y, c=label, s=10, cmap=cmap, norm=norm)
+
     cb = plt.colorbar(scat, spacing='proportional', ticks=bounds)
 
 
 if __name__ == "__main__":
-    pca()
+    dataset = load_wine(return_X_y=True)
+
     func = [
         NaiveBayes,
         GaussianClassifier,
-
+        pca
     ]
     for x in func:
         print(x)
-        x(load_breast_cancer(return_X_y=True), False)
+        x(dataset, show=True)
